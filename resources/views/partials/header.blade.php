c86d7e42bcd97b6ed88ca24ce93b8a72a01fab03
Aranel9966 <99angelo.dire@gmail.com>
2023-05-09 15:59:50 +0200

    add layout and partials, add route and config

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <nav>
+        <div class="logo">
+            <img src="{{Vite::asset('resources/img/favicon.ico')}}" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            <li >aaaaaaaaaaaaaaaaaaaaa</li>
+            </ul>
+        </div>
+    </nav>
+    <div id="jumbotron"></div>
+</header>
